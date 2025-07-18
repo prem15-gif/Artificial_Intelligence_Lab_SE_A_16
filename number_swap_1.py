d7c33ca7315f735a0ca3270c2cac510649a9be0f
Chaitanya Zarekar <vaishnavi.munde24@mmit.edu.in>
2025-07-18 10:19:17 +0530

    THIS CAN SWAP NUMBERS

---
+++ number_swap_1.py
@@ -0,0 +1,10 @@
+# Taking two numbers as input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Swapping the numbers
+num1, num2 = num2, num1
+
+# Display the swapped numbers
+print(f"After swapping: \nFirst number: {num1}\nSecond number: {num2}")
+
